788a359a011539138d903263fc585e869aad87d1
datdnt <datdnt@vietmap.vn>
2025-04-21 12:36:51 +0700

    add payload noti

--- lib/core/alice_core.dart
+++ lib/core/alice_core.dart
@@ -235,7 +235,7 @@ class AliceCore {
       'Alice (total: ${callsSubject.value.length} requests)',
       message,
       platformChannelSpecifics,
-      payload: '',
+      payload: 'alice_custom',
     );
     _notificationMessageShown = message;
     _notificationProcessing = false;
