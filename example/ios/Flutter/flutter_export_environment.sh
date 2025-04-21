1826e323faf119dd2d2bca97a023085db1da0096
datdnt <datdnt@vietmap.vn>
2025-04-21 10:27:40 +0700

    update

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\2024FlutterInstall\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Khanh Katsu\Desktop\alice-master\package\example"
+export "FLUTTER_ROOT=/Users/DATDNT/fvm/versions/3.22.1"
+export "FLUTTER_APPLICATION_PATH=/Users/DATDNT/Desktop/Code/Test-Demo/custom_alice/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
